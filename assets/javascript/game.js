0b983ea63e506b3f50828a33d348779341101523
David Gallegos <david.gallegos@outlook.com>
2019-03-10 12:48:06 -0700

    IDs weren't linked, messed some things up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,37 @@
 /* varaibles for computer choices and user choices
 also for links with html <screen. Reference Rockpapersciss5*/
-var alphaThings = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-'o','p','q','r','s','t','u','v','w','x','y','z'];
+var alphaThings = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var win = 0;
 var losses = 0;
-var ties = 0;
-
+var guessleft = 10;
+var guessEd = []
 
 var winsTex = document.getElementById("wins-text");
 var lossTex = document.getElementById("lose-text");
-var guessText= document.getElementById("guess-text");
-var soFarText = document.getElementById("soFar");
+var guessesText = document.getElementById("guess-text");
+var guessEdText = document.getElementById("soFar");
 
 
 var computerGuess = alphaThings[Math.floor(Math.random() * alphaThings.length)];
-console.log (computerGuess)
+console.log(computerGuess)
 //create functions
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
-// variables used within the fuction
+    // variables used within the fuction
     var userGuess = event.key;
-    console.log (userGuess)
-        
-// create the if, else if and else 
-    if (userGuess===computerGuess) {
-        win++;;
-    } else {
-        losses++;
-    }
-    
-//display the goods
-winsTex = "You won fool" + win;
-lossTex = "You lost moron!" + losses;
-ties = "We don't like ties"
+    console.log(userGuess)
 
+    // create the if, else if and else 
 
+    if (userGuess === computerGuess) {
+        win++;
+         //I'M GOING CRAZY WITH LINE 30 AND 29 ERRORS. it was working on the html, now i screwed it up!
+    } else (userGuess != computerGuess)
+        losses--;
 
-
-
-
-    
+    //display the goods
+    winsTex.textContent = "You won fool = " + win;
+    lossTex.textContent = "You lost moron! = " + losses;
 }
-
-
